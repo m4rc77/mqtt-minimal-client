9db9edfa4998dcdf150d63a2188eca89b3337d30
Marc Schmid (mschmid) <mschmid@puzzle.ch>
2020-11-29 21:12:10 +0100

    Get rid of the . on the output

--- src/main/java/ch/m4rc/mqtt/MqttMinimalClient.java
+++ src/main/java/ch/m4rc/mqtt/MqttMinimalClient.java
@@ -52,7 +52,6 @@ public class MqttMinimalClient {
 
             //noinspection InfiniteLoopStatement
             while (true) {
-                System.out.println(".");
                 sleep();
             }
 
